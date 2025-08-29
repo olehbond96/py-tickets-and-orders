a183eb060dfdcb9a318d09c80ab1a5ecd936c2f1
Oleh Bondarchuk <127667913+olehbond96@users.noreply.github.com>
2025-08-29 15:21:56 +0200

    Solution

--- services/movie.py
+++ services/movie.py
@@ -1,20 +1,12 @@
 from django.db.models import QuerySet
-
 from db.models import Movie
+from django.db import transaction
 
 
-def get_movies(
-    genres_ids: list[int] = None,
-    actors_ids: list[int] = None,
-) -> QuerySet:
+def get_movies(title: str = None) -> QuerySet:
     queryset = Movie.objects.all()
-
-    if genres_ids:
-        queryset = queryset.filter(genres__id__in=genres_ids)
-
-    if actors_ids:
-        queryset = queryset.filter(actors__id__in=actors_ids)
-
+    if title:
+        queryset = queryset.filter(title__icontains=title)
     return queryset
 
 
@@ -22,6 +14,7 @@ def get_movie_by_id(movie_id: int) -> Movie:
     return Movie.objects.get(id=movie_id)
 
 
+@transaction.atomic
 def create_movie(
     movie_title: str,
     movie_description: str,
---
+++ services/order.py
@@ -0,0 +1,41 @@
+from typing import List, Dict, Any, Optional
+from django.db import transaction
+from django.shortcuts import get_object_or_404
+from .user import get_user
+from db.models import User, Order, Ticket, MovieSession
+
+
+@transaction.atomic
+def create_order(
+    tickets: List[Dict[str, Any]],
+    username: str,
+    date: Optional[str] = None
+) -> Order:
+    user = get_object_or_404(User, username=username)
+
+    order_data = {"user": user}
+    if date:
+        order_data["created_at"] = date
+
+    order = Order.objects.create(**order_data)
+
+    for ticket_data in tickets:
+        movie_session_id = ticket_data.get("movie_session")
+        movie_session = get_object_or_404(MovieSession, pk=movie_session_id)
+
+        Ticket.objects.create(
+            movie_session=movie_session,
+            order=order,
+            row=ticket_data.get("row"),
+            seat=ticket_data.get("seat")
+        )
+
+    return order
+
+
+def get_orders(username: Optional[str] = None) -> List[Order]:
+    orders = Order.objects.all()
+    if username:
+        user = get_user(username)
+        orders = orders.filter(user=user)
+    return list(orders)
