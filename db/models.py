5856d89d110536e8ca471f175a1fcf94b4afc034
Oleh Bondarchuk <127667913+olehbond96@users.noreply.github.com>
2025-08-30 21:32:25 +0200

    Solution

--- db/models.py
+++ db/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, PermissionsMixin, Group, Permission
-from django.db.models import F
 from django.core.exceptions import ValidationError
-from django.db.transaction import atomic
 
 
 class User(AbstractUser, PermissionsMixin):
